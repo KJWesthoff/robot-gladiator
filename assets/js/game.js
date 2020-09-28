6a3a3c4dcac51b7544356212dbc911911879f184
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-09-28 11:00:25 -0700

    added declarations to js.game file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("Jadda jadda JavaScript")
+// fight function
+
+function fight() {
+    window.alert("The fight has started!");
+}
+
+var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName,playerHealth ,playerAttack)
+
+var enemyName = "Roborto"
+var enemyHealth = 50;
+var enemyAttack = 10;
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+}
+
+fight();
