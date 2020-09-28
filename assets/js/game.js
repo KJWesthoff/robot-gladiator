30b8a800c906d1ea70e69221f5d1581a93bd5964
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-09-28 11:20:55 -0700

    fight function updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,26 @@ var enemyHealth = 50;
 var enemyAttack = 10;
 
 var fight = function(){
+    // alert users that they are starting the gameround
     window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of playerAttack from the vaĺue of enemyHealth, use result to update the value in the enemyHealth variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know it works
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        ); 
+
+
+    //Subtract the value of enemyAttack from the value of playerHealth, use the result to update the value in the playerHealth var
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know it works
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        ); 
+
 }
 
 fight();
