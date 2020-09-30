6e129dd5d95a534189e5bf9dff93e841542741d7
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-09-29 18:03:21 -0700

    round numbers added

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,7 +68,12 @@ var fight = function (enemyName) {
 
 // Main loop over enemies
 for (var i =0;i<enemyNames.length; i++){
-    
+    if(playerHealth > 0){
+        window.alert("Welcome to Robot Gladiators! Round " + ( i+1));
+    }else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     //set enemy variables
     var pickedEnemyName = enemyNames[i]; 
     enemyHealth = 50;
